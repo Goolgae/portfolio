2aa10fbce8833f4e94eb839b2abd77eb3a341180
Goolgae <sunwoo0706@icloud.com>
2020-11-17 21:20:18 +0900

    fix js file

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,64 +1,78 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
+  // 메뉴바
   // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
+  $(".menu-toggle").click(function (e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
+      "fa-bars fa-times"
+    );
     $(this).toggleClass("active");
   });
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+  $("#sidebar-wrapper .js-scroll-trigger").click(function () {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
+      "fa-bars fa-times"
+    );
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
+      $(".scroll-to-top").fadeIn();
     } else {
-      $('.scroll-to-top').fadeOut();
+      $(".scroll-to-top").fadeOut();
     }
   });
-
 })(jQuery); // End of use strict
+
+/** 이 밑은 삭제된 맵 기능입니다. **/
 
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
+// var onMapMouseleaveHandler = function (event) {
+// var that = $(this);
+// that.on("click", onMapClickHandler);
+// that.off("mouseleave", onMapMouseleaveHandler);
+// that.find("iframe").css("pointer-events", "none");
+// };
+// var onMapClickHandler = function (event) {
+// var that = $(this);
+// Disable the click handler until the user leaves the map area
+// that.off("click", onMapClickHandler);
+// Enable scrolling zoom
+// that.find("iframe").css("pointer-events", "auto");
+// Handle the mouse leave event
+// that.on("mouseleave", onMapMouseleaveHandler);
+// };
 // Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+// $(".map").on("click", onMapClickHandler);
